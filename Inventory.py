2d7c0cb14414ff78da7784caa189fe2a2c4263b3
Alexand3r12345 <alexander.angman@elev.ga.ntig.se>
2025-11-07 09:25:19 +0100

    25-11-07

--- Inventory.py
+++ Inventory.py
@@ -1,19 +1,44 @@
 run = True
-bag = []
+bag = ["katt", "majs"]
 print("Välkommen till påsen😎")
+
+
 while run:
     print("Visa innehållet i påsen [V]🤪")
     print("Spara i påsen [S]🧐")
     print("Avsluta programmet [Q]🥲")
     print("ta bort saker från inventory[D]")
+    print("Sök efter saker i påsen [F]")
     choice = input("Välj: ")
     if choice.lower() == "v":
-        for thing in bag:
-            print(thing)
+        if not bag:
+            print("Påsen är tom")
+        else:
+            for thing in bag:
+                print(thing)
     elif choice.lower() == "s":
         bag.append(input("Skriv vad du vill spara: "))
         
     elif choice.lower() == "q":
         run = False
+    elif choice.lower() == "f":
+        query = input("vad vill du söka efter?")
+        if query.lower() in bag:
+            print(f"hittade: {query} i påsen")
+        else:
+            print(f"du sökte efter {query}, men det finns inte i påsen")
+    elif choice.lower() == "d":
+        if not bag: 
+            print("påsen är tom")
+        else:
+            item = input("vad vill du ta bort")
+       
+        if item in bag:
+            bag.remove(item)
+            print(f"du har tagit bort {item} från din lista")
+       
+    
+
+
     else:
         print("Felaktigt kommando, försök igen.")
